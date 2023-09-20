955ce2326964b44662791e5662fc8aa6f2ef9c03
Willy <willykipchumba22@gmail.com>
2023-09-20 17:12:19 +0300

    completed assignmets

---
+++ index.js
@@ -0,0 +1,24 @@
+const divide = (a=2000,b =100) => {
+
+    let  c = a /b
+   return c
+   }
+   
+   divide(2000,100)
+   
+   
+   const square = (a) => {
+   
+   let b = a* a
+   
+   return b
+   }
+   
+   
+   const add = (a, b) => {
+   
+   let c = a+ b
+   
+   return c
+   
+   }
